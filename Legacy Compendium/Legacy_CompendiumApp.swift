0e4679ba7154526aa5b555a301435bf556d44258
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2023-02-20 13:53:39 -0300

    Initial Commit

---
+++ Legacy Compendium/Legacy_CompendiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  Legacy_CompendiumApp.swift
+//  Legacy Compendium
+//
+//  Created by Jacob Aguilar on 20-02-23.
+//
+
+import SwiftUI
+
+@main
+struct Legacy_CompendiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
