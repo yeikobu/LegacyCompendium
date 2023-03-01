9493ecc3dc5b545cfc7449ebf423108ad82264ab
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2023-03-01 18:39:02 -0300

    Add completed transition from card spell view to Spell extended card view

--- Legacy Compendium/Views/SpellsView/Components/SpellCardView.swift
+++ Legacy Compendium/Views/SpellsView/Components/SpellCardView.swift
@@ -10,11 +10,6 @@ import Kingfisher
 
 struct SpellCardView: View {
     
-//    @State var imgUrl: String
-//    @State var spellName: String
-//    @State var spellType: String
-//    @State var coolDown: String
-//    @State var description: String
     @State var spellModel: SpellsAndCharm
     var animation: Namespace.ID
     
@@ -22,17 +17,19 @@ struct SpellCardView: View {
         ZStack(alignment: .top) {
             CardBackgroundView()
                 .shadow(color: Color(.black).opacity(0.2), radius: 5)
-                .matchedGeometryEffect(id: "background\(spellModel.spellName)", in: animation)
+//                .matchedGeometryEffect(id: "background\(spellModel.spellName)", in: animation)
             
             KFImage(URL(string: spellModel.img))
                 .resizable()
                 .scaledToFit()
                 .cornerRadius(10)
+                .matchedGeometryEffect(id: "image\(spellModel.spellName)", in: animation)
                 .frame(width: 60)
                 .offset(y: -25)
                 .overlay(
                     RoundedRectangle(cornerRadius: 10)
                         .stroke(Color("SelectedOptionBorder").opacity(spellModel.spellName == "Basic Cast" ? 0 : 1), lineWidth: 2)
+                        .matchedGeometryEffect(id: "image\(spellModel.spellName)border", in: animation)
                         .offset(y: -25)
                 )
             
@@ -41,36 +38,40 @@ struct SpellCardView: View {
                     .font(.custom("CrimesOfGrindelwald", size: 20))
                     .multilineTextAlignment(.center)
                     .foregroundColor(Color("SelectedOptionBorder"))
+                    .matchedGeometryEffect(id: "spellName\(spellModel.spellName)", in: animation)
                     .padding(.top, 22)
                     .padding(.horizontal, 5)
                 
                 VStack {
                     HStack(spacing: 5)  {
                         Text("Type")
-                            .font(.custom("UniversityOldstyleBook", size: 14))
+                            .font(.custom("UniversityOldstyleBook", size: 16))
                             .foregroundColor(.gray)
                         
                         Text(spellModel.type)
-                            .font(.custom("UniversityOldstyleBook", size: 14))
+                            .font(.custom("UniversityOldstyleBook", size: 16))
                             .foregroundColor(.purple)
                             .multilineTextAlignment(.leading)
                     }
+                    .matchedGeometryEffect(id: "type\(spellModel.spellName)", in: animation)
                     
                     HStack(spacing: 5) {
                         Text("Cooldown")
-                            .font(.custom("UniversityOldstyleBook", size: 14))
+                            .font(.custom("UniversityOldstyleBook", size: 16))
                             .foregroundColor(.gray)
                         
                         Text(spellModel.cooldown)
-                            .font(.custom("UniversityOldstyleBook", size: 14))
+                            .font(.custom("UniversityOldstyleBook", size: 16))
                             .foregroundColor(.white)
                     }
+                    .matchedGeometryEffect(id: "cooldown\(spellModel.spellName)", in: animation)
                 }
                 .padding(.top, 5)
                 .padding(.horizontal, 5)
                 
                 Text(spellModel.description)
-                    .font(.custom("UniversityOldstyleBook", size: 14))
+                    .font(.custom("UniversityOldstyleBook", size: 16))
+                    .matchedGeometryEffect(id: "description\(spellModel.spellName)", in: animation)
                     .foregroundColor(Color("SelectedOptionBorder"))
                     .padding(.horizontal, 5)
                     .padding(.top, 10)
@@ -80,9 +81,9 @@ struct SpellCardView: View {
             .padding(10)
             .padding(.top, 10)
         }
-        .padding(.vertical, 10)
-        .frame(width: 220, height: 250)
         .matchedGeometryEffect(id: "allView\(spellModel.spellName)", in: animation)
+        .padding(.vertical, 30)
+        .frame(width: 220, height: 270)
     }
 }
 
@@ -92,6 +93,7 @@ struct SpellCardView_Previews: PreviewProvider {
     @State static private var spellModel = SpellsAndCharm(spellName: "Accio", type: "Force", cooldown: "8 Seconds", unlock: "Main Quest - Charms Class", description: "Yanks most enemies to you, and pulls them airborne. Used in exploration to grab collectibles.", upgrades: "Also lifts enemies around target into air.", img: "https://firebasestorage.googleapis.com/v0/b/legacy-helper.appspot.com/o/Spells%2Faccio.avif?alt=media&token=0e12d0f8-1db0-4e36-acd6-44499272e023")
     
     static var previews: some View {
-        SpellCardView(spellModel: spellModel, animation: animation)
+//        SpellCardView(spellModel: spellModel, animation: animation)
+        DashboardView(isTransitionActive: .constant(false))
     }
 }
--- Legacy Compendium/Views/SpellsView/Components/SpellExtendedCardVIew.swift
+++ Legacy Compendium/Views/SpellsView/Components/SpellExtendedCardVIew.swift
@@ -11,42 +11,135 @@ import Kingfisher
 struct SpellExtendedCardVIew: View {
     
     @State var spellModel: SpellsAndCharm
+    @Binding var showContent: Bool
     var animation: Namespace.ID
     
     var body: some View {
-        ZStack {
-            Color("Background")
-            
             ZStack {
                 CardBackgroundView()
                     .shadow(color: Color(.black).opacity(0.2), radius: 5)
-                    .matchedGeometryEffect(id: "background\(spellModel.spellName)", in: animation)
                 
                 KFImage(URL(string: spellModel.img))
                     .resizable()
                     .scaledToFit()
                     .cornerRadius(10)
-                    .frame(width: 80)
-                    .offset(y: -25)
+                    .matchedGeometryEffect(id: "image\(spellModel.spellName)", in: animation)
+                    .frame(width: 100)
+                    .offset(y: -240)
                     .overlay(
                         RoundedRectangle(cornerRadius: 10)
                             .stroke(Color("SelectedOptionBorder").opacity(spellModel.spellName == "Basic Cast" ? 0 : 1), lineWidth: 2)
-                            .offset(y: -25)
+                            .matchedGeometryEffect(id: "image\(spellModel.spellName)border", in: animation)
+                            .offset(y: -240)
                     )
+                
+                VStack {
+                    Text(spellModel.spellName)
+                        .font(.custom("CrimesOfGrindelwald", size: 20))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color("SelectedOptionBorder"))
+                        .matchedGeometryEffect(id: "spellName\(spellModel.spellName)", in: animation)
+                        .padding(.top, 72)
+                        .padding(.horizontal, 5)
+                    
+                    HStack {
+                        HStack(spacing: 5)  {
+                            Text("Type")
+                                .font(.custom("UniversityOldstyleBook", size: 16))
+                                .foregroundColor(.gray)
+                            
+                            Text(spellModel.type)
+                                .font(.custom("UniversityOldstyleBook", size: 16))
+                                .foregroundColor(.purple)
+                                .multilineTextAlignment(.leading)
+                        }
+                        .matchedGeometryEffect(id: "type\(spellModel.spellName)", in: animation)
+                        
+                        Spacer()
+                        
+                        HStack(spacing: 5) {
+                            Text("Cooldown")
+                                .font(.custom("UniversityOldstyleBook", size: 16))
+                                .foregroundColor(.gray)
+                            
+                            Text(spellModel.cooldown)
+                                .font(.custom("UniversityOldstyleBook", size: 16))
+                                .foregroundColor(.white)
+                        }
+                        .matchedGeometryEffect(id: "cooldown\(spellModel.spellName)", in: animation)
+                    }
+                    .padding(.top, 5)
+                    .padding(.horizontal, 10)
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        if showContent {
+                            Text("Description")
+                                .font(.custom("UniversityOldstyleBook", size: 16))
+                                .foregroundColor(.gray)
+                                .opacity(showContent ? 1 : 0)
+                        }
+                        
+                        Text(spellModel.description)
+                            .font(.custom("UniversityOldstyleBook", size: 16))
+                            .foregroundColor(Color("SelectedOptionBorder"))
+                            .matchedGeometryEffect(id: "description\(spellModel.spellName)", in: animation)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 10)
+                    .padding(.horizontal, 10)
+                    
+                    if showContent {
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text("How to get")
+                                .font(.custom("UniversityOldstyleBook", size: 16))
+                                .foregroundColor(.gray)
+                                .opacity(showContent ? 1 : 0)
+                            
+                            Text(spellModel.unlock)
+                                .font(.custom("UniversityOldstyleBook", size: 16))
+                                .foregroundColor(Color("SelectedOptionBorder"))
+                        }
+                        .matchedGeometryEffect(id: "unlock\(spellModel.spellName)", in: animation)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.top, 10)
+                        .padding(.horizontal, 10)
+                        
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text("Upgrades")
+                                .font(.custom("UniversityOldstyleBook", size: 16))
+                                .foregroundColor(.gray)
+                                .opacity(showContent ? 1 : 0)
+                            
+                            Text(spellModel.upgrades)
+                                .font(.custom("UniversityOldstyleBook", size: 16))
+                                .foregroundColor(Color("SelectedOptionBorder"))
+                        }
+                        .matchedGeometryEffect(id: "upgrades\(spellModel.spellName)", in: animation)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.top, 10)
+                        .padding(.horizontal, 10)
+                    }
+                    
+                    Spacer()
+                }
             }
-            .frame(width: 320, height: 380)
             .matchedGeometryEffect(id: "allView\(spellModel.spellName)", in: animation)
-        }
-        .ignoresSafeArea()
+            .frame(width: 320, height: 480)
+            .onAppear {
+                withAnimation(.easeInOut(duration: 0.35).delay(0.2)){
+                    showContent = true
+                }
+            }
     }
 }
 
 struct SpellExtendedCardVIew_Previews: PreviewProvider {
     
-    @State static private var spellModel = SpellsAndCharm(spellName: "Accio", type: "Force", cooldown: "8 Seconds", unlock: "Main Quest - Charms Class", description: "Yanks most enemies to you, and pulls them airborne. Used in exploration to grab collectibles.", upgrades: "Also lifts enemies around target into air.", img: "https://firebasestorage.googleapis.com/v0/b/legacy-helper.appspot.com/o/Spells%2Faccio.avif?alt=media&token=0e12d0f8-1db0-4e36-acd6-44499272e023")
+    @State static private var spellModel = SpellsAndCharm(spellName: "Accio", type: "Force", cooldown: "8 Seconds", unlock: "Main Quest - Charms Class", description: "Yanks most enemies to you, and pulls ", upgrades: "Also lifts enemies around target into air.", img: "https://firebasestorage.googleapis.com/v0/b/legacy-helper.appspot.com/o/Spells%2Faccio.avif?alt=media&token=0e12d0f8-1db0-4e36-acd6-44499272e023")
     @Namespace static var animation
     
     static var previews: some View {
-        SpellExtendedCardVIew(spellModel: spellModel, animation: animation)
+//        SpellExtendedCardVIew(spellModel: spellModel, animation: animation)
+        ContentView()
     }
 }
